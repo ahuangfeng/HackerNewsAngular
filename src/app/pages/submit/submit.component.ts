5f677b903f4542a485e4143c8b1985005d26c10b
paugm96 <pau.gonzalez.montiel@est.fib.upc.edu>
2018-05-29 09:54:58 +0200

    submit control inputs(still not working)

--- src/app/pages/submit/submit.component.ts
+++ src/app/pages/submit/submit.component.ts
@@ -6,10 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit.component.css']
 })
 
+
 export class SubmitComponent implements OnInit {
 
-  changeTitle(title) {
-    console.log(title);
+  title;
+  url;
+  ask;
+  errorMessage;
+
+
+
+  changeTitle(newValue) {
+    this.title = newValue;
+  }
+
+  changeUrl(newValue) {
+    this.url = newValue;
+  }
+
+  changeAsk(newValue) {
+    this.ask = newValue;
+  }
+
+  createContribution() {
+    if(this.ask !== undefined && this.url !== undefined) {
+      this.errorMessage = "Unable to create, you must choose between type url or type ask";
+      document.getElementById("p").style.visibility = "visible";
+    }
+    if (this.ask === undefined && this.url !== undefined) {
+      //TODO: contribution type url
+    }
+
+    else if(this.ask !== undefined && this.url === undefined) {
+      //TODO: contribution type ask
+    }
+
+    else {
+      //TODO: message error
+      this.errorMessage = "You must enter an url or an ask.";
+      document.getElementById("p").style.visibility = "visible";
+
+    }
+
+
+
+    /*console.log(this.title.target.value);
+    console.log(this.url.target.value);
+    console.log(this.ask.target.value);*/
+
   }
   constructor() { }
 
