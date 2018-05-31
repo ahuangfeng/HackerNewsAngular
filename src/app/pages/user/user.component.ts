9bad8471e9b407132934afa0f116b20f9f413b75
carlavarea <carla.varea@est.fib.upc.edu>
2018-05-31 18:51:09 +0200

    user id

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpService } from 'src/app/providers/http.service';
 import { User } from '../../model/user';
@@ -10,11 +12,10 @@ import { User } from '../../model/user';
 export class UserComponent implements OnInit {
 
   @Input() user: User;
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService, private route: ActivatedRoute) { }
 
-  
   ngOnInit() {
-    this.httpService.getUser("1").then(data => {
+    this.httpService.getUser(this.getUserId()).then(data => {
       console.log("data", data);
       this.user = data['user'];
     }).catch(err => {
@@ -22,4 +23,7 @@ export class UserComponent implements OnInit {
     })
   }
 
+  getUserId(): string{
+    return this.route.snapshot.paramMap.get('id');
+  }
 }
