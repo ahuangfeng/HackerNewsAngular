501714c0a2a3593f8ad3da3051005eec7f1f459e
Alex Huang Feng <alexhuang05@gmail.com>
2018-06-01 21:21:05 +0200

    style delete edit contribution

--- src/app/components/single-contribution/single-contribution.component.ts
+++ src/app/components/single-contribution/single-contribution.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit, Input } from '@angular/core';
 import { Contribution } from '../../model/contribution';
+import { AuthService } from '../../providers/auth.service';
 
 @Component({
   selector: 'single-contribution',
@@ -13,10 +14,30 @@ export class SingleContributionComponent implements OnInit {
 
   contributionShow: Contribution;
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit() {
     this.contributionShow = new Contribution(this.contribution);
+  }
+
+  ownContribution(){
+    return this.authService.currentUserID == this.contributionShow.user_id;
+  }
+
+  delete(){
+    console.log("TODO: delete");
+  }
+
+  edit(){
+    console.log("TODO: Edit;");
+  }
+
+  vote(){
+    console.log("TODO: Vote contribution:", this.contributionShow.id);
+  }
+
+  unvote(){
+    console.log("TODO: UnVote contribution:", this.contributionShow.id);
   }
 
 }
